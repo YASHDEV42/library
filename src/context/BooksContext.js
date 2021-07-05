ce6b7f890590bf90bd53fda2a990b0e70baa697c
Yahya Shannat <yahyashannat@gmail.com>
2021-07-05 18:44:44 +0300

    ok

--- src/context/BooksContext.js
+++ src/context/BooksContext.js
@@ -13,7 +13,7 @@ export const BooksProvider=(props)=>{
         {title:"geometry puza", img: geometryPuza, link:"https://www.mediafire.com/file/vgx2vpqdmfn7wjw/geometry_puza.pdf/file"},
         {title:"math 1 K metropol", img: math1MetropolK, link:"https://www.mediafire.com/file/f5gaywhtpo60ir1/math-1_konu.pdf/file"},
         {title:"math 1 SB metropol", img: math1MetropolSB, link:"https://www.mediafire.com/file/ma9xmstcql2bxoa/math-1-SB_metropol.pdf/file"},
-        {title:"math 2 K metropol", img:math2MetropolK, link:'',},
+        {title:"math 2 K metropol", img:math2MetropolK, link:'https://www.mediafire.com/file/ws2xphyto6vx5sb/math-2-K_metropol.pdf/file',},
     ])
     return(
         <BooksContext.Provider value={{books,setBooks}}>
